175b5d5a6c602396443eda73fa40dde09e88dfe4
Liam Goss <liam@liam.goss.com>
2024-03-19 18:29:21 -0700

    Converted indents

--- Luli/Pico/read_uv.py
+++ Luli/Pico/read_uv.py
@@ -43,7 +43,7 @@ UV_SENSITIVITY = 2300 # From datasheet
 UVI_TO_UW_CM2 = 25  # 1 UVI = 25 µW/cm²
 
 class LTR390:
-    def __init__(self, address=ADDR):
+	def __init__(self, address=ADDR):
 		self.i2c = I2C(id=0, scl=Pin(1), sda=Pin(0), freq=400000)
 		self.i2c.writeto_mem(address, LTR390_MAIN_CTRL, bytearray([0x02]))
 		self.address = address
@@ -59,54 +59,54 @@ class LTR390:
 		self.Write_Byte(LTR390_GAIN, GAIN_18) #  Gain Range=3.
 		# self.Write_Byte(LTR390_INT_CFG, 0x34) # UVS_INT_EN=1, Command=0x34
 		# self.Write_Byte(LTR390_GAIN, GAIN_3) #  Resolution=18bits, Meas Rate = 100ms
-        
-    def Read_Byte(self, cmd):
-        rdate = self.i2c.readfrom_mem(int(self.address), int(cmd), 1)
-        return rdate[0]
+		
+	def Read_Byte(self, cmd):
+		rdate = self.i2c.readfrom_mem(int(self.address), int(cmd), 1)
+		return rdate[0]
 
-    def Write_Byte(self, cmd, val):
-        self.i2c.writeto_mem(int(self.address), int(cmd), bytes([int(val)]))
-        
-    def UVS(self):
-        # self.Write_Byte(LTR390_MAIN_CTRL, 0x0A) #  UVS in Active Mode
-        Data1 = self.Read_Byte(LTR390_UVSDATA)
-        Data2 = self.Read_Byte(LTR390_UVSDATA + 1)
-        Data3 = self.Read_Byte(LTR390_UVSDATA + 2)
-        uv =  (Data3 << 16)| (Data2 << 8) | Data1
-        # UVS = Data3*65536+Data2*256+Data1
-        # print("UVS = ", UVS)
-        return uv
-    
-    def UVI(self):
-        raw_uv = self.UVS()
-        WFAC = 1  # WFAC = 1 for no window/glass, WFAC > 1 for tinted window
-        uvi = (raw_uv / UV_SENSITIVITY) * WFAC
-        return uvi, raw_uv
+	def Write_Byte(self, cmd, val):
+		self.i2c.writeto_mem(int(self.address), int(cmd), bytes([int(val)]))
+		
+	def UVS(self):
+		# self.Write_Byte(LTR390_MAIN_CTRL, 0x0A) #  UVS in Active Mode
+		Data1 = self.Read_Byte(LTR390_UVSDATA)
+		Data2 = self.Read_Byte(LTR390_UVSDATA + 1)
+		Data3 = self.Read_Byte(LTR390_UVSDATA + 2)
+		uv =  (Data3 << 16)| (Data2 << 8) | Data1
+		# UVS = Data3*65536+Data2*256+Data1
+		# print("UVS = ", UVS)
+		return uv
+	
+	def UVI(self):
+		raw_uv = self.UVS()
+		WFAC = 1  # WFAC = 1 for no window/glass, WFAC > 1 for tinted window
+		uvi = (raw_uv / UV_SENSITIVITY) * WFAC
+		return uvi, raw_uv
    
-    def UV_intensity(self):
-        # convert raw UV data to microwatts per square centimeter (µW/cm^2)
-        raw_uv = self.UVS()
-        uvi = raw_uv / UV_SENSITIVITY
-        uv_intensity = uvi * UVI_TO_UW_CM2
-        return uv_intensity
+	def UV_intensity(self):
+		# convert raw UV data to microwatts per square centimeter (µW/cm^2)
+		raw_uv = self.UVS()
+		uvi = raw_uv / UV_SENSITIVITY
+		uv_intensity = uvi * UVI_TO_UW_CM2
+		return uv_intensity
 
 
-    
+	
 # ========= End LTR390 UV sensor driver =============
 if __name__ == '__main__':
-    sensor = LTR390()
-    utime.sleep(1)
-    try:
-        while True:
-            #UVI, raw = sensor.UVI()
-            UV_level = sensor.UV_intensity()
-            print("UV Intensity: {:.2f} µW/cm²".format(UV_level))
-            utime.sleep(0.5)
-            
-    except KeyboardInterrupt:
-        exit()
-        
-        
+	sensor = LTR390()
+	utime.sleep(1)
+	try:
+		while True:
+			#UVI, raw = sensor.UVI()
+			UV_level = sensor.UV_intensity()
+			print("UV Intensity: {:.2f} µW/cm²".format(UV_level))
+			utime.sleep(0.5)
+			
+	except KeyboardInterrupt:
+		exit()
+		
+		
 """UV Intensity: 0.00 µW/cm²
 UV Intensity: 0.00 µW/cm²
 UV Intensity: 0.41 µW/cm²
